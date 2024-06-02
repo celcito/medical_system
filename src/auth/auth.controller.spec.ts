962f476a7c9db4a415869111d09b8ceb63bd1101
celcito <celso832@gmail.com>
2024-06-02 01:11:48 -0300

    feat: tests auth

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/users.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService, // Fornecer o AuthService real
+        {
+          provide: UsersService,
+          useValue: {
+            findOneByEmail: jest.fn().mockResolvedValue({
+              email: "testqqtss333@acme.com",
+              password: "@7777Celcito4",
+            }),
+          },
+        },
+        {
+          provide: getRepositoryToken(User),
+          useClass: User,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+
+    // Espiar o método signIn do AuthService e definir o comportamento do mock
+    jest.spyOn(authService, 'signIn').mockImplementation((email, password) =>
+      Promise.resolve({
+        email,
+        password,
+      })
+    );
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should handle successful sign in', async () => {
+    await controller.signIn({
+      email: "testqqtss333@acme.com",
+      password: "@7777Celcito",
+    });
+
+    expect(authService.signIn).toHaveBeenCalledWith(
+       "testqqtss333@acme.com", "@7777Celcito",
+    );
   });
+
+
 });
+
+
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/users.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findOneByEmail: jest.fn().mockResolvedValue(null),
+          },
+        },
+        {
+          provide: getRepositoryToken(User),
+          useClass: User,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +30,6 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+
 });
