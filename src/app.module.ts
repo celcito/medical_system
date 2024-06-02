6a0f49c52932a9396ed2590e0012dff4c8ced07b
celcito <celso832@gmail.com>
2024-06-01 23:35:30 -0300

    feat: users crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule} from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 
+
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '172.27.0.2',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'celcito',
+    migrations: ['./migrations/*.ts'],
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize:true,
+    migrationsRun: true,
+  }),UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
