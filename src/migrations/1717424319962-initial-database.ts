9b5328d3d4fa5cf624c9a662fb504d04899657ce
celcito <celso832@gmail.com>
2024-06-03 15:17:27 -0300

    chore: add migrations to create table user and add field birhtdate

---
+++ src/migrations/1717424319962-initial-database.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialDatabase1717424319962 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE  IF NOT EXISTS user (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                firstName VARCHAR(255) NULL,
+                lastName VARCHAR(255) NULL,
+                password VARCHAR(255) NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE user`);
+    }
+
+}
