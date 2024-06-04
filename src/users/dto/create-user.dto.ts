b62d3a766df8c9e87511ce69c3915fde7d2dc804
celcito <celso832@gmail.com>
2024-06-03 22:40:28 -0300

    refactor: remove item not used

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,25 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsEmail, Length,IsDate} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { PasswordComplexity } from '../../helpers/custom.password.validator'
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
 
-  @Length(8, 20)
+  @Length(8)
+
+  @PasswordComplexity( {
+    message: 'The password needs to have at least 8 digits, and at least 1 numeric character and one special character.',
+  })
   password: string;
 
   @Length(3)
   firstName: string;
   @Length(3)
   lastName: string;
+  @Transform(({ value }) => new Date(value))
+
+  @IsDate()
+  birthDate:Date;
+
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body,  Req, Res, Get, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Body,  Req, Res, Get, Param, Delete, ParseIntPipe} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,7 +8,7 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Post()
-  async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+  async create(@Body(new ValidationPipe({transform:true})) createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 
